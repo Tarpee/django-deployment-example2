a3650c3902b2c3c6eec8711c1856159666c859c6
autentico.jason <autentico.jason@gmail.com>
2018-10-20 15:41:14 +0800

    'first'

---
+++ learning_templates/basic_app/templatetags/my_extras.py
@@ -0,0 +1,13 @@
+from django import template
+
+register =template.Library()
+
+@register.filter(name='cut')
+def cut(value,arg):
+    """
+    cut out all values of arg
+    """
+
+    return value.replace(arg,'')
+
+    register.filter('cut',cut)
